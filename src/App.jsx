78cc7b648d07e7c487773ad4b257ce263528075a
alvarezdap <alvarezdap@pm.me>
2023-04-16 14:22:09 -0500

    Se creo un punto de referencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from "axios"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    //Cuando se usa fetch se sobreentiende que es GET
+    //Tengo dos formas de traer info de una API a continuacion
+    //Con axios obtenemos un objeto y podemos hacer mas que GET
+    //Con axios obtenemos tambien el status del pedido
+    //Con fetch solo hacemos GET
+    axios.get(`${API_URL}assets`)
+    .then((data) => {
+      setCriptos(data.data.data)
+      
+    /*
+    fetch(`${API_URL}assets`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCriptos(data.data)
+    })
+    .catch(() => {
+      console.error("La peticion fallo")
+    */
+    })
+  }, [])
+
+if (!criptos) return <span>Cargando...</span>
 
   return (
-    <h1>Hola Mundo</h1>
+    <>
+    <h1>Lista de Criptomonedas</h1>
+    <ol>
+    { 
+      criptos.map(({id, name, priceUsd}) => (
+        <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+      )) 
+    }
+    </ol>
+    </>
   )
 }
 
